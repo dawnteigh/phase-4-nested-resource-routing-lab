c2a1b3a84afb9e74e56db6f588dca584f75030ae
Donte Ladatto <donte279@gmail.com>
2022-07-10 13:21:55 -0500

    commit tree 86bc9e1c31d8d73aa5e3a5acf853df4d31f55aa9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def create
+    user = find_user
+    new_item = user.items.create(item_params)
+    render json: new_item, status: :created
+  end
+
+  def show
+    item = find_user.items.find(params[:id])
+    render json: item, status: :ok
+  end
+    
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def not_found_response
+    render json: { error: "Resource not found" }, status: :not_found
   end
 
 end
